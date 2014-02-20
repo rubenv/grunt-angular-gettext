37c4b58a3c9f69029988cd3451be6726f6c59a0d
Ruben Vermeersch <ruben@rocketeer.be>
2014-02-20 18:56:46 +0100

    Switch compile over to angular-gettext-tools.

--- tasks/compile.js
+++ tasks/compile.js
@@ -1,27 +1,5 @@
+var compile = require('angular-gettext-tools').compile;
 var po = require('pofile');
-
-var formats = {
-    javascript: {
-        addLocale: function (locale, strings) {
-            return "    gettextCatalog.setStrings('" + locale + "', " + (JSON.stringify(strings)) + ");\n";
-        },
-        format: function (locales, options) {
-            return "angular.module(\"" + options.module + "\").run(['gettextCatalog', function (gettextCatalog) {\n" + locales.join('') + "\n}]);";
-        }
-    },
-    json: {
-        addLocale: function (locale, strings) {
-            return {name: locale, strings: strings};
-        },
-        format: function (locales, options) {
-            var result = {};
-            locales.forEach(function (locale) {
-                result[locale.name] = locale.strings;
-            });
-            return JSON.stringify(result);
-        }
-    }
-};
 
 module.exports = function (grunt) {
     grunt.registerMultiTask('nggettext_compile', 'Compile strings from .po files', function () {
@@ -30,33 +8,17 @@ module.exports = function (grunt) {
             module: 'gettext'
         });
 
-        if (!formats.hasOwnProperty(options.format)) {
+        if (!compile.hasFormat(options.format)) {
             throw new Error('There is no "' + options.format + '" output format.');
         }
 
-        var format = formats[options.format];
 
         this.files.forEach(function (file) {
-            var locales = [];
-
-            file.src.forEach(function (input) {
-                var data = grunt.file.read(input);
-                var catalog = po.parse(data);
-
-                if (!catalog.headers.Language) {
-                    throw new Error('No Language header found!');
-                }
-
-                var strings = {};
-                for (var i = 0; i < catalog.items.length; i++) {
-                    var item = catalog.items[i];
-                    strings[item.msgid] = item.msgstr.length === 1 ? item.msgstr[0] : item.msgstr;
-                }
-
-                locales.push(format.addLocale(catalog.headers.Language, strings));
+            var inputs = file.src.map(function (input) {
+                return grunt.file.read(input);
             });
 
-            grunt.file.write(file.dest, format.format(locales, options));
+            grunt.file.write(file.dest, compile.convertPo(inputs, options));
         });
     });
 };
